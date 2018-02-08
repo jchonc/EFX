dcb3f2ac4d4345341bc146eb43ca071b3903d662
Jian <jchonc@gmail.com>
2018-02-08 11:51:47 -0500

    xml driven form engine is working!

---
+++ EFX.Web/FormController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using React;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EFX.Web
+{
+    public class FormController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        [Route("forms/{formName}")]
+        public object GetTranspiledForm(string formName)
+        {
+            //Ideally this should be from database...
+            string formContent = @"
+<MetaForm readOnly={{ false }}>
+    <MetaField path=""name"" />
+</MetaForm >";
+
+            string fakeSource = $"class {formName} extends React.Component {{ render() {{ return ({formContent}); }} }}";
+            var environment = React.ReactEnvironment.Current;
+            var result = environment.Babel.Transform(fakeSource);
+            return Content(result, "text/javascript");
+        }
+
+    }
+}
